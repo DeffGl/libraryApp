a3caee1d2098bca0fa7d4c09b270d5db239da85f
DeffGI <holldeevv@gmail.com>
2024-11-02 13:20:35 +0700

    Removing unnecessary files and packages.

--- src/main/java/com/example/libraryapp/exceptions/localization/LocalizationNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.libraryapp.exceptions.localization;
-
-public class LocalizationNotFoundException extends Exception{
-    public LocalizationNotFoundException(String message) {
-        super(message);
-    }
-}
